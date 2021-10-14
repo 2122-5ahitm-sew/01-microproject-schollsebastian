5fb5a21a37c02206591593cc58c0c7aa380a5374
schollsebastian <scholl.sebastian@gmx.at>
2021-10-14 23:16:44 +0200

    Make Customer a Hibernate Entity

--- backend/src/main/java/at/htl/fakturierung/entity/Customer.java
+++ backend/src/main/java/at/htl/fakturierung/entity/Customer.java
@@ -1,6 +1,15 @@
 package at.htl.fakturierung.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
     public String firstName;
     public String lastName;
     public String street;
@@ -10,7 +19,8 @@ public class Customer {
 
     public Customer() {}
 
-    public Customer(String firstName, String lastName, String street, int houseNumber, int zip, String city) {
+    public Customer(Long id, String firstName, String lastName, String street, int houseNumber, int zip, String city) {
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.street = street;
@@ -19,10 +29,67 @@ public class Customer {
         this.city = city;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(int houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
     @Override
     public String toString() {
         return "Customer{" +
-                "firstName='" + firstName + '\'' +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 ", street='" + street + '\'' +
                 ", houseNumber=" + houseNumber +
