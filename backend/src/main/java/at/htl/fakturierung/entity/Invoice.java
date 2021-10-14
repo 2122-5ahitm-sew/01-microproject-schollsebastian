569fbe728222324e23cf6da8cd7f9412f8c6d3eb
schollsebastian <scholl.sebastian@gmx.at>
2021-10-14 23:17:21 +0200

    Make Invoice a Hibernate Entity

--- backend/src/main/java/at/htl/fakturierung/entity/Invoice.java
+++ backend/src/main/java/at/htl/fakturierung/entity/Invoice.java
@@ -1,18 +1,47 @@
 package at.htl.fakturierung.entity;
 
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+import javax.persistence.*;
+
+@Entity
 public class Invoice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    @ManyToOne
+    @Cascade(CascadeType.ALL)
     public Customer customer;
 
     public Invoice() {}
 
-    public Invoice(Customer customer) {
+    public Invoice(Long id, Customer customer) {
+        this.id = id;
+        this.customer = customer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
         this.customer = customer;
     }
 
     @Override
     public String toString() {
         return "Invoice{" +
-                "customer=" + customer +
+                "id=" + id +
+                ", customer=" + customer +
                 '}';
     }
 }
