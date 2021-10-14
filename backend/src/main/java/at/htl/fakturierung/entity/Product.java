da7fff400e7faf1276cef449720dcb62fa50631a
schollsebastian <scholl.sebastian@gmx.at>
2021-10-14 23:17:34 +0200

    Make Product a Hibernate Entity

--- backend/src/main/java/at/htl/fakturierung/entity/Product.java
+++ backend/src/main/java/at/htl/fakturierung/entity/Product.java
@@ -1,20 +1,55 @@
 package at.htl.fakturierung.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
     public String name;
     public double price;
 
     public Product() {}
 
-    public Product(String name, double price) {
+    public Product(Long id, String name, double price) {
+        this.id = id;
         this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
         this.price = price;
     }
 
     @Override
     public String toString() {
         return "Product{" +
-                "name='" + name + '\'' +
+                "id=" + id +
+                ", name='" + name + '\'' +
                 ", price=" + price +
                 '}';
     }
