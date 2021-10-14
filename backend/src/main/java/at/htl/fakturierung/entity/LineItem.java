9eaa6b20600888361ccdf187872916ffd46901cd
schollsebastian <scholl.sebastian@gmx.at>
2021-10-14 23:17:26 +0200

    Make LineItem a Hibernate Entity

--- backend/src/main/java/at/htl/fakturierung/entity/LineItem.java
+++ backend/src/main/java/at/htl/fakturierung/entity/LineItem.java
@@ -1,22 +1,69 @@
 package at.htl.fakturierung.entity;
 
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+import javax.persistence.*;
+
+@Entity
 public class LineItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    @ManyToOne
+    @Cascade(CascadeType.ALL)
     public Product product;
+    @ManyToOne
+    @Cascade(CascadeType.ALL)
     public Invoice invoice;
     public int amount;
 
     public LineItem() {}
 
-    public LineItem(Product product, int amount, Invoice invoice) {
+    public LineItem(Long id, Product product, Invoice invoice, int amount) {
+        this.id = id;
         this.product = product;
-        this.amount = amount;
         this.invoice = invoice;
+        this.amount = amount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Invoice getInvoice() {
+        return invoice;
+    }
+
+    public void setInvoice(Invoice invoice) {
+        this.invoice = invoice;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
     }
 
     @Override
     public String toString() {
         return "LineItem{" +
-                "product=" + product +
+                "id=" + id +
+                ", product=" + product +
                 ", invoice=" + invoice +
                 ", amount=" + amount +
                 '}';
